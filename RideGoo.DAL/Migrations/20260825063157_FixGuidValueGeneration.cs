7a069ee62adfbb542169c92bbcf27db7d2e15093
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-08-25 11:49:40 +0500

    bacand bitd

---
+++ RideGoo.DAL/Migrations/20260825063157_FixGuidValueGeneration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RideGoo.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGuidValueGeneration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
