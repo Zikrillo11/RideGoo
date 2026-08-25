d3802fb004b02d7e2e455ca119beb44c620707d3
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-08-25 11:58:17 +0500

    sd

---
+++ RideGoo.Api/Configuration/CorsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace RideGoo.Api.Configuration;
+
+public static class CorsConfiguration
+{
+    private const string PolicyName = "RideGooWebPolicy";
+
+    public static IServiceCollection AddCorsConfiguration(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(PolicyName, policy =>
+            {
+                policy.WithOrigins("http://localhost:5173", "https://localhost:5173") // Vite dev server manzili
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials();
+            });
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseCorsConfiguration(this IApplicationBuilder app)
+    {
+        return app.UseCors(PolicyName);
+    }
+}
